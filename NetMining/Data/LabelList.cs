524d697ba21ba5c1d5a483c34a2cefaf516acb97
Jeff Borwey <jeffborwey@gmail.com>
2015-04-03 20:13:57 -0500

    External Evaluation now stores the results.  Added some ease of use functionality to LabelList.

--- NetMining/Data/LabelList.cs
+++ NetMining/Data/LabelList.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using NetMining.ClusteringAlgo;
+using NetMining.Files;
 
 namespace NetMining.Data
 {
     public class LabelList
     {
         public readonly int[] LabelIndices;
-        public readonly List<String> UniqueLabels; 
+        public readonly List<String> UniqueLabels;
+
+        public LabelList(DelimitedFile f, int col) : this(f.GetColumn(col))
+        {
+            
+        }
+
         public LabelList(String[] labels)
         {
             LabelIndices = new int[labels.Length];
@@ -20,6 +28,23 @@ namespace NetMining.Data
                 }
                 LabelIndices[i] = UniqueLabels.IndexOf(labels[i]);
             }
+        }
+
+        public int[,] GetMatching(Partition p)
+        {
+            //create a count mapping
+            //[actual cluster label, number in found clusters]
+            int[,] clusterMatching = new int[UniqueLabels.Count, p.Clusters.Count];
+            foreach (Cluster c in p.Clusters)
+            {
+                foreach (ClusteredItem k in c.Points)
+                {
+                    int actualMatching = LabelIndices[k.Id];
+                    int foundMatching = k.ClusterId;
+                    clusterMatching[actualMatching, foundMatching]++;
+                }
+            }
+            return clusterMatching;
         }
     }
 }
--- NetMining/Evaluation/ExternalEval.cs
+++ NetMining/Evaluation/ExternalEval.cs
@@ -1,17 +1,54 @@
 ﻿
 using System;
+using System.Collections.Generic;
 using System.Text;
 using NetMining.ClusteringAlgo;
 using NetMining.Data;
 
 namespace NetMining.Evaluation
 {
-    public static class ExternalEval
+    public class ExternalEval
     {
-        public static String GreedyErrorEval(Partition clusterFile, LabelList labels, int[,] clusterMatching)
+        public double TotalAccuracy;
+        public int TotalMatched;
+        public int TotalSize;
+        public List<GroundTruthMatch> Matches; 
+        public readonly Partition P;
+        public readonly LabelList L;
+
+        public class GroundTruthMatch
         {
-            int truthCount = labels.UniqueLabels.Count;
-            int partitionCount = clusterFile.Clusters.Count;
+            public readonly int PartitionClusterId;
+            public readonly int GroundTruthClusterId;
+            public readonly String GroundTruthLabel;
+            public readonly int NumMatched;
+            public readonly int TotalSize;
+            public readonly double Accuracy;
+
+            public GroundTruthMatch(int partId, int gtId, int nMatched, int size, double accuracy, String label)
+            {
+                PartitionClusterId = partId;
+                GroundTruthClusterId = gtId;
+                GroundTruthLabel = label;
+                NumMatched = nMatched;
+                TotalSize = size;
+                Accuracy = accuracy;
+            }
+        }
+
+        public ExternalEval(Partition clusterFile, LabelList labels)
+        {
+            P = clusterFile;
+            L = labels;
+            Matches = new List<GroundTruthMatch>();
+            int[,] clusterMatching = labels.GetMatching(clusterFile);
+            GreedyErrorEval(clusterMatching);
+        }
+
+        private void GreedyErrorEval(int[,] clusterMatching)
+        {
+            int truthCount = L.UniqueLabels.Count;
+            int partitionCount = P.Clusters.Count;
             int[] assigned = new int[truthCount];
             bool[] ourCIsAssigned = new bool[partitionCount];
             for (int i = 0; i < partitionCount; i++)
@@ -41,22 +78,26 @@ namespace NetMining.Evaluation
 
                 if (assigned[realC] == 0)
                 {
-                    sb.AppendFormat("Label {0} was not assigned", labels.UniqueLabels[realC]);
+                    Matches.Add(new GroundTruthMatch(-1, realC, 0, sumRealC, 0, L.UniqueLabels[realC]));
+                    sb.AppendFormat("Label {0} was not assigned", L.UniqueLabels[realC]);
                 }
                 else
                 {
                     ourCIsAssigned[assignedClust] = true;
                     sb.AppendFormat("Cluster {0} Assigned to Label {1} Accuracy: ({2}/{3}) {4}%",
-                        assignedClust, labels.UniqueLabels[realC], assigned[realC], sumRealC, 100.0 * (double)assigned[realC] / (double)sumRealC);
+                        assignedClust, L.UniqueLabels[realC], assigned[realC], sumRealC, 100.0 * (double)assigned[realC] / (double)sumRealC);
+                    Matches.Add(new GroundTruthMatch(assignedClust, realC, assigned[realC], sumRealC, (double)assigned[realC] / sumRealC, L.UniqueLabels[realC]));
                 }
 
                 sb.AppendLine();
                 sumCorrect += assigned[realC];
             }
+            TotalMatched = sumCorrect;
+            TotalSize = P.DataCount;
+            TotalAccuracy = (double) sumCorrect/ P.DataCount;
 
-            sb.AppendFormat("Total Accuracy: ({0}/{1}) {2}%", sumCorrect, clusterFile.DataCount, 100.0 * (double)sumCorrect / (double)clusterFile.DataCount);
+            sb.AppendFormat("Total Accuracy: ({0}/{1}) {2}%", sumCorrect, P.DataCount, 100.0 * (double)sumCorrect / (double)P.DataCount);
             sb.AppendLine();
-            return sb.ToString();
         }
 
         private static String OptimalErrorEval(Partition clusterFile, LabelList labels, int[,] clusterMatching)
