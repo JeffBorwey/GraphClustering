1c8d51a8d8adcfe5889f92ecef244af23ba67352
JeffBorwey <jeffborwey@gmail.com>
2015-04-09 15:40:00 -0500

    Added a 2nd Random Graph Model using normalized proababilities

--- NetMining/Graphs/Generator/RandomGraphGenerator.cs
+++ NetMining/Graphs/Generator/RandomGraphGenerator.cs
@@ -18,7 +18,7 @@ namespace NetMining.Graphs.Generator
     {
         private int _alpha = 3;
         private int _expP = 1;
-
+        private bool _useNormalizedProb = false;
 
         /// <summary>
         /// Sets alpha for constructing a Random Graph
@@ -40,13 +40,98 @@ namespace NetMining.Graphs.Generator
         }
 
         /// <summary>
+        /// This sets the generative model used for creating a graph 
+        /// </summary>
+        /// <param name="val"></param>
+        public void UseNormalizedProb(bool val)
+        {
+            _useNormalizedProb = val;
+        }
+
+        /// <summary>
         /// Generates a Random graph using the defined paramaters
         /// </summary>
         /// <param name="d">The DistanceMatrix used for construction</param>
         /// <returns></returns>
         public LightWeightGraph GenerateGraph(DistanceMatrix d)
         {
-            return GetRandomGraph(d, _alpha, _expP);
+            if (!_useNormalizedProb)
+                return GetRandomGraph(d, _alpha, _expP);
+            return GetNormalizedRandomGraph(d);
+        }
+
+        public LightWeightGraph GetNormalizedRandomGraph(DistanceMatrix d)
+        {
+            int numNodes = d.Count;
+            var nodes = new LightWeightGraph.LightWeightNode[numNodes];
+            // make an array to hold all possible edges, less the edges in the mst
+            oneNode[] myDistances = new oneNode[numNodes * (numNodes - 1) / 2 - (numNodes - 1)];
+
+            LightWeightGraph mst = LightWeightGraph.GetStackedMST(d, 1);
+            LightWeightGraph.LightWeightNode[] mstNodes = mst.Nodes;
+            int myDistancesIndex = 0;
+
+            //Create a list to hold edge values
+            List<int>[] edges = new List<int>[numNodes];
+            List<float>[] weights = new List<float>[numNodes];
+            for (int i = 0; i < numNodes; i++)
+                edges[i] = new List<int>();
+
+            // add edges from the mst to the edges list, to facilitate adding additional edges later on
+            for (int i = 0; i < numNodes; i++)
+            {
+                for (int j = 0; j < mstNodes[i].Edge.Length; j++)
+                {
+                    edges[i].Add(mstNodes[i].Edge[j]);
+                }
+            }
+
+            double minDist = double.MaxValue;
+            for (int i = 0; i < numNodes - 1; i++)
+                for (int j = i + 1; j < numNodes; j++)
+                    minDist = Math.Min(d[i, j], minDist);
+                    // cycle through each possible edge
+                    // if the edge exists in the mst, continue
+                    // otherwise, add the edge to distances array, and add distance to the edge to the cummulative total
+            
+            for (int i = 0; i < numNodes - 1; i++)
+            {
+                for (int j = i + 1; j < numNodes; j++)
+                    if (mstNodes[i].Edge.Contains(j))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        double addlProb = 1.0 / Math.Pow(d[i, j]/minDist, _expP);
+                        oneNode nd = new oneNode { prob = addlProb, fromNode = i, toNode = j, alreadyExists = false };
+                        myDistances[myDistancesIndex] = nd;
+                        myDistancesIndex++;
+                    }
+            }
+
+
+                // walk through the array until you find the random number
+            for (int m = 0; m < myDistances.Length; m++)
+            {
+                double rand = Utility.Util.Rng.NextDouble();
+                if (myDistances[m].prob > rand)
+                {
+                    // we have found the edge to add.
+                    // add the edge if it does not already exist
+                    if (!myDistances[m].alreadyExists)
+                    {
+                        edges[myDistances[m].fromNode].Add(myDistances[m].toNode);
+                        edges[myDistances[m].toNode].Add(myDistances[m].fromNode);
+                        myDistances[m].alreadyExists = true;
+                        break;
+                    }
+                }
+            }
+            for (int i = 0; i < numNodes; i++)
+                nodes[i] = new LightWeightGraph.LightWeightNode(i, true, edges[i], weights[i]);
+
+            return new LightWeightGraph(nodes, true);
         }
 
         /// <summary>
