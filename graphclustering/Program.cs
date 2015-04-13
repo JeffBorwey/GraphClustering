c2a943d36721aa9bfccda0e7a7a536931fa7ca06
Jeff Borwey <jeffborwey@gmail.com>
2015-04-12 22:19:45 -0500

    Removed commented debug code from GUI source

--- graphclustering/Program.cs
+++ graphclustering/Program.cs
@@ -15,25 +15,10 @@ namespace GraphClustering
         [STAThread]
         static void Main()
         {
-            //LightWeightGraph lwg = Form1.getGraphFromFile("iris/iris.txt0.graph");
-
-            //LightWeightGraph lwg = Form1.getGraphFromFile("Swiss\\swiss.graph");
-            //lwg.isWeighted = false;
-            //lwg.saveGML("iris.gml");
-            //get betweenss
-            //Stopwatch sw = new Stopwatch();
-            //sw.Start();
-            //ClusterAlgo.VAT v = new ClusterAlgo.VAT(lwg);
-            //sw.Stop();
-            //MessageBox.Show(sw.ElapsedMilliseconds.ToString());
-            //return;
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
             Form1 f = new Form1();
-
-
 
             Application.Run(f);
         }
